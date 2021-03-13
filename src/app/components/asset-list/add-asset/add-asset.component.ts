8e1dc48761669137c0d015428b73a9e8a210827c
karuna <karuna.cogniphi@gmail.com>
2021-03-12 23:02:31 -0800

    header active and create asset changes

--- src/app/components/asset-list/add-asset/add-asset.component.ts
+++ src/app/components/asset-list/add-asset/add-asset.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'add-asset',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAssetComponent implements OnInit {
 
-  constructor() { }
+  categoryArray = [
+    { id: 0, name: 'Select Category' },
+    { id: 1, name: 'Branch 1' },
+    { id: 2, name: 'Branch 2' },
+    { id: 3, name: 'Branch 3' },
+  ];
+  priorityArray = [
+    { id: 0, name: 'Select Priority' },
+    { id: 1, name: 'High' },
+    { id: 2, name: 'Medium' },
+    { id: 3, name: 'Low' },
+  ];
+  typeArray = [
+    { id: 0, name: 'Select Type' },
+    { id: 1, name: 'Fixed' },
+    { id: 2, name: 'None' },
+  ];
+  statusArray = [
+    { id: 0, name: 'Select Status' },
+    { id: 1, name: 'Active' },
+    { id: 2, name: 'Inactive' },
+  ];
+  createAssetForm: FormGroup;
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createAssetForm = this.formbuilder.group({
+      id: [1225],
+      name: [''],
+      category: [],
+      value: [''],
+      priority: [],
+      assignedTo: [''],
+      type: [''],
+      status: [''],
+    });
   }
 
+  createAsset(formData) {
+    console.log('formData', formData);
+  }
 }
